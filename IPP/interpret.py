a2351cd23beb44625f936431f9b6d681a2571dcf
Shchepa <andrewbandit228@gmail.com>
2022-03-05 18:00:45 +0100

    76% yes yes yes

--- IPP/interpret.py
+++ IPP/interpret.py
@@ -36,6 +36,7 @@ class Program:
         self._data_stack = []
         self._stats = Stats()
         self._input_f = None
+        self._order = [0] # change in case
 
     def get_line(self):
         return self._line
@@ -76,21 +77,23 @@ class Instruction():
 
     def check_frame_none(self, frame):
         if frame == None:
-            #print("PIZDAAAAAAAAAA")
             exit(FRAME_ERR)
 
     def _get_var(self, var, prg, key):
         if key != "var":
             return None, key, var
 
+        #print(var)
+        if var[0:2] == "LF":
+            self.check_frame_none(prg._LF)
+        elif var[0:2] == "TF":
+            self.check_frame_none(prg._TF)
         try:
             if var[0:2] == "LF":
-                self.check_frame_none(prg._LF)
                 type_value = prg._LF[var[3:]]
             elif var[0:2] == "GF":
                 type_value = prg._GF[var[3:]]
             elif var[0:2] == "TF":
-                self.check_frame_none(prg._TF)
                 type_value = prg._TF[var[3:]]
         except:
             return None, None, None
@@ -168,6 +171,7 @@ class Popframe(Instruction):
             exit(FRAME_ERR)
 
         prg._TF = prg._frames.pop()
+        prg._LF = prg._frames[-1]
 
 class Break(Instruction):
     def __init__(self, dict_args):
@@ -213,8 +217,8 @@ class Jump(Instruction):
         try:
             prg.set_line(prg._labels_dict[value] - 1)
         except:
-            print("Label doenst exist")
-            exit(6547)
+            #print("Label doenst exist")
+            exit(SEMANTIC_ERR)
 
 class Pushs(Instruction):
     def __init__(self, dict_args):
@@ -344,8 +348,13 @@ class Eq(Instruction):
         frame2, name2, typ2, value2 = get_values(self, 1)
         frame3, name3, typ3, value3 = get_values(self, 2)
 
-        check_type(typ1, typ2, typ3, typ2)
-        self._add_var(name1, frame1, "bool", str(value2 == value3).lower(), prg)
+        if typ2 != typ3 and typ2 != "nil" and typ3 != "nil":
+            exit(TYPES_ERR)
+
+        if typ1 == None or typ1 == "bool":
+            self._add_var(name1, frame1, "bool", str(value2 == value3).lower(), prg)
+        else:
+            exit(TYPES_ERR)
 
 class Or(Instruction):
     def __init__(self, dict_args):
@@ -362,7 +371,7 @@ class Or(Instruction):
         frame3, name3, typ3, value3 = get_values(self, 2)
 
         check_type(typ1, typ2, typ3, "bool")
-        self._add_var(name1, frame1, "bool", str(bool(value2) * bool(value3)).lower(), prg)
+        self._add_var(name1, frame1, "bool", str(str2bool(value2) or str2bool(value3)).lower(), prg)
 
 class Concat(Instruction):
     def __init__(self, dict_args):
@@ -483,7 +492,7 @@ class Return(Instruction):
             #print("VUT FIT U ERR")
             exit(NOVALUE_ERR)
 
-        prg.set_line(prg._return_lines.pop() - 1)
+        prg.set_line(prg._return_lines.pop())
 
 class Strlen(Instruction):
     def __init__(self, dict_args):
@@ -550,7 +559,7 @@ class Dprint(Instruction):
         num = super().get_num_of_arg()
 
     def execute(self, prg):
-        frame, name, typ, value = get_values(self)
+        frame, name, typ, value = get_values(self, 0)
 
         value = value if "string" != typ else re.sub(r'\\([0-9]{3})', lambda x: chr(int(x[1])), value)
         print("" if typ == "nil" else value, end='', file=sys.stderr)
@@ -668,7 +677,7 @@ class And(Instruction):
         frame3, name3, typ3, value3 = get_values(self, 2)
 
         check_type(typ1, typ2, typ3, "bool")
-        self._add_var(name1, frame1, "bool", str(bool(value2) * bool(value3)).lower(), prg)
+        self._add_var(name1, frame1, "bool", str(str2bool(value2) and str2bool(value3)).lower(), prg)
 
 class Not(Instruction):
     def __init__(self, dict_args):
@@ -684,7 +693,7 @@ class Not(Instruction):
         frame2, name2, typ2, value2 = get_values(self, 1)
 
         check_2_keys(typ1, "bool", typ2, "bool")
-        self._add_var(name1, frame1, "bool", str(not bool(value2)).lower(), prg)
+        self._add_var(name1, frame1, "bool", str(not str2bool(value2)).lower(), prg)
 
 class Getchar(Instruction):
     def __init__(self, dict_args):
@@ -719,8 +728,7 @@ class Read(Instruction):
         num = super().get_num_of_arg()
 
     def execute(self, prg):
-        line = input() if prg._input_f is None else prg._input_f.readline()
-        line = line.rtrim()
+        line = input() if prg._input_f is None else prg._input_f.readline().rtrim()
 
         (key, value), = self.get_arg(1).items()
         frame1, name1, typ1, value1 = get_values(self, 0)
@@ -736,6 +744,321 @@ class Read(Instruction):
         if value == '':
             self._add_var(name1, frame1, "nil", "nil", prg)
 
+class AddS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("ADDS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        if len(prg._data_stack) < 2:
+            #print("ERORO POP")
+            exit(NOVALUE_ERR)
+
+        type_stack1, value_stack1 = prg._data_stack.pop()
+        type_stack2, value_stack2 = prg._data_stack.pop()
+
+        if type_stack1 != type_stack2 or type_stack1 != "int":
+            #print("SEM_ERR pop")
+            exit(TYPES_ERR)
+
+        prg._data_stack.append((type_stack1, int(value_stack1) + int(value_stack2)))
+
+class SubS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("SUBS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        if len(prg._data_stack) < 2:
+            #print("ERORO POP")
+            exit(NOVALUE_ERR)
+
+        type_stack1, value_stack1 = prg._data_stack.pop()
+        type_stack2, value_stack2 = prg._data_stack.pop()
+
+        if type_stack1 != type_stack2 or type_stack1 != "int":
+            #print("SEM_ERR pop")
+            exit(TYPES_ERR)
+
+        prg._data_stack.append((type_stack1, int(value_stack1) - int(value_stack2)))
+
+class MulS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("MULS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        if len(prg._data_stack) < 2:
+            #print("ERORO POP")
+            exit(NOVALUE_ERR)
+
+        type_stack1, value_stack1 = prg._data_stack.pop()
+        type_stack2, value_stack2 = prg._data_stack.pop()
+
+        if type_stack1 != type_stack2 or type_stack1 != "int":
+            #print("SEM_ERR pop")
+            exit(TYPES_ERR)
+
+        prg._data_stack.append((type_stack1, int(value_stack1) * int(value_stack2)))
+
+class IdivS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("IDIVS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        if len(prg._data_stack) < 2:
+            #print("ERORO POP")
+            exit(NOVALUE_ERR)
+
+        type_stack1, value_stack1 = prg._data_stack.pop()
+        type_stack2, value_stack2 = prg._data_stack.pop()
+
+        if type_stack1 != type_stack2 or type_stack1 != "int":
+            #print("SEM_ERR pop")
+            exit(TYPES_ERR)
+
+        if int(value_stack2) == 0:
+            #print("Psel nahuy")
+            exit(WRONG_VALUE_ERR)
+
+        prg._data_stack.append((type_stack1, int(value_stack1) // int(value_stack2)))
+
+class ClearS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("CLEARS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        prg._data_stack = []
+
+class JumpifeqS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("JUMPIFEQS", 1, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        (key, value), = self.get_arg(0).items()
+        type_stack1, value_stack1 = prg._data_stack.pop()
+        type_stack2, value_stack2 = prg._data_stack.pop()
+
+        if type_stack1 != type_stack2:
+            #print("TYPES_ERR")
+            exit(TYPES_ERR)
+
+        try:
+            if value_stack1 == value_stack2:
+                prg.set_line(prg._labels_dict[value] - 1)
+        except:
+            #print("Label doenst exist")
+            exit(SEMANTIC_ERR)
+
+class JumpifneqS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("JUMPIFNEQS", 1, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        (key, value), = self.get_arg(0).items()
+        type_stack1, value_stack1 = prg._data_stack.pop()
+        type_stack2, value_stack2 = prg._data_stack.pop()
+
+        if type_stack1 != type_stack2:
+            #print("TYPES_ERR")
+            exit(TYPES_ERR)
+
+        try:
+            if value_stack1 != value_stack2:
+                prg.set_line(prg._labels_dict[value] - 1)
+        except:
+            #print("Label doenst exist")
+            exit(SEMANTIC_ERR)
+
+class NotS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("NOTS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        if len(prg._data_stack) < 1:
+            #print("ERORO POP")
+            exit(NOVALUE_ERR)
+
+        type_stack1, value_stack1 = prg._data_stack.pop()
+
+        if type_stack1 != "bool":
+            #print("SEM_ERR pop")
+            exit(TYPES_ERR)
+
+        prg._data_stack.append(("bool", str(not str2bool(type_stack1)).lower()))
+
+class AndS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("ANDS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        if len(prg._data_stack) < 2:
+            #print("ERORO POP")
+            exit(NOVALUE_ERR)
+
+        type_stack1, value_stack1 = prg._data_stack.pop()
+        type_stack2, value_stack2 = prg._data_stack.pop()
+
+        if type_stack1 != type_stack2 or type_stack1 != "bool":
+            #print("SEM_ERR pop")
+            exit(TYPES_ERR)
+
+        prg._data_stack.append(("bool", str(str2bool(value_stack1) and str2bool(value_stack2)).lower()))
+
+class OrS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("ORS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        if len(prg._data_stack) < 2:
+            #print("ERORO POP")
+            exit(NOVALUE_ERR)
+
+        type_stack1, value_stack1 = prg._data_stack.pop()
+        type_stack2, value_stack2 = prg._data_stack.pop()
+
+        if type_stack1 != type_stack2 or type_stack1 != "bool":
+            #print("SEM_ERR pop")
+            exit(TYPES_ERR)
+
+        prg._data_stack.append(("bool", str(str2bool(value_stack1) or str2bool(value_stack2)).lower()))
+
+class EqS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("EQS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        if len(prg._data_stack) < 2:
+            #print("ERORO POP")
+            exit(NOVALUE_ERR)
+
+        type_stack1, value_stack1 = prg._data_stack.pop()
+        type_stack2, value_stack2 = prg._data_stack.pop()
+
+        if type_stack1 != type_stack2 and type_stack1 != "nil" and type_stack2 != "nil":
+            #print("SEM_ERR pop")
+            exit(TYPES_ERR)
+
+        prg._data_stack.append(("bool", str(value_stack1 == value_stack2).lower()))
+
+class LtS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("LTS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        if len(prg._data_stack) < 2:
+            #print("ERORO POP")
+            exit(NOVALUE_ERR)
+
+        type_stack1, value_stack1 = prg._data_stack.pop()
+        type_stack2, value_stack2 = prg._data_stack.pop()
+
+        if type_stack1 != type_stack2:
+            #print("SEM_ERR pop")
+            exit(TYPES_ERR)
+
+        prg._data_stack.append(("bool", str(value_stack1 < value_stack2).lower()))
+
+class GtS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("GTS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        if len(prg._data_stack) < 2:
+            #print("ERORO POP")
+            exit(NOVALUE_ERR)
+
+        type_stack1, value_stack1 = prg._data_stack.pop()
+        type_stack2, value_stack2 = prg._data_stack.pop()
+
+        if type_stack1 != type_stack2:
+            #print("SEM_ERR pop")
+            exit(TYPES_ERR)
+
+        prg._data_stack.append(("bool", str(value_stack1 > value_stack2).lower()))
+
+class Int2charS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("INT2CHARS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        if len(prg._data_stack) < 1:
+            #print("ERORO POP")
+            exit(NOVALUE_ERR)
+
+        type_stack1, value_stack1 = prg._data_stack.pop()
+
+        if type_stack1 != "int":
+            exit(TYPES_ERR)
+
+        if 0 <= int(value_stack1) <= 1114111:
+            prg._data_stack.append(("string", str(chr(value_stack1)).lower()))
+        else:
+            #print("Nejaka chybaaaa")
+            exit(STRING_ERR)
+
+class Stri2intS(Instruction):
+    def __init__(self, dict_args):
+        super().__init__("STRI2INTS", 0, dict_args)
+
+    def check_args(self):
+        num = super().get_num_of_arg()
+
+    def execute(self, prg):
+        if len(prg._data_stack) < 2:
+            #print("ERORO POP")
+            exit(NOVALUE_ERR)
+
+        type_stack1, value_stack1 = prg._data_stack.pop()
+        type_stack2, value_stack2 = prg._data_stack.pop()
+
+        if type_stack1 != "string" or type_stack1 != "int":
+            exit(TYPES_ERR)
+        try:
+            prg._data_stack.append(("string", str(ord(value_stack1[value_stack2])).lower()))
+        except:
+            #print("Mimo pole")
+            exit(STRING_ERR)
+
 class Factory:
     _dict_func = {"DEFVAR"  : Defvar , "CREATEFRAME" : Createframe, "POPFRAME" : Popframe,
                   "BREAK"   : Break  , "CALL"        : Call       , "JUMP"     : Jump    ,
@@ -748,7 +1071,12 @@ class Factory:
                   "WRITE"   : Write  , "TYPE"        : Type       , "IDIV"     : Idiv    ,
                   "READ"    : Read   , "AND"         : And        , "GT"       : Gt      ,
                   "PUSHS"   : Pushs  , "MUL"         : Mul        , "SUB"      : Sub     ,
-                  "SETCHAR" : Setchar, "NOT"         : Not
+                  "SETCHAR" : Setchar, "NOT"         : Not        , "ADDS"     : AddS    ,
+                  "CLEARS"  : ClearS , "JUMPIFEQS"   : JumpifeqS  , "SUBS"     : SubS    ,
+                  "NOTS"    : NotS   , "JUMPIFNEQS"  : JumpifneqS , "MULS"     : MulS    ,
+                  "IDIVS"   : IdivS  , "INT2CHARS"   : Int2charS  , "LTS"      : LtS     ,
+                  "GTS"     : GtS    , "STRI2INTS"   : Stri2intS  , "EQS"      : EqS     ,
+                  "ANDS"    : AndS   , "ORS"         : OrS
                   }
 
     @classmethod
@@ -758,6 +1086,9 @@ class Factory:
         except:
             exit(STRUCT_ERR)
             #print("ERROR with name of instruction, add exit error")
+
+def str2bool(val):
+    return True if val == "true" else False;
 
 def where_is_name(name):
     if name == None:
@@ -790,10 +1121,18 @@ def check_label(prg, opcode, dict):
 
         prg._labels_dict[dict["label"]] = prg.get_num_of_instrs()
 
-def check_order(prg, order):
-    if prg.get_num_of_instrs() != int(order):
-        #print("ER UUUUU")
+def check_order(prg, order, curr_instr):
+    try:
+        if int(order) < 1:
+            #print("ER UUUUU")
+            exit(STRUCT_ERR)
+    except ValueError:
         exit(STRUCT_ERR)
+
+    if int(order) in prg._order or int(order) < prg._order[curr_instr]:
+        exit(STRUCT_ERR)
+
+    prg._order.append(int(order))
 
 def inc_insts(prg, opcode):
     if opcode not in ('LABEL', 'DPRINT', 'BREAK'):
@@ -812,24 +1151,28 @@ try:
     for opt, arg in opts:
         if opt in ('--help'):
             if len(sys.argv) != 2:
-                exit(10)
+                exit(PARAM_ERR)
             #print(help)
             exit(0)
         elif opt in ('--source'):
             if tree != None:
                 exit(PARAM_ERR)
+
             try:
                 tree = ET.parse(arg)
-            except:
-                #print("Nowm file zdaj")
+            except FileNotFoundError:
                 exit(PARAM_ERR)
+            except ET.ParseError:
+                exit(FORMAT_ERR)
+
         elif opt in ('--input'):
             if prg._input_f != None:
                 exit(PARAM_ERR)
+
             try:
                 prg._input_f = open(arg, "r")
             except:
-                print("Nowm file zdaj")
+                #print("Nowm file zdaj")
                 exit(PARAM_ERR)
         else:
             #print("ERR with argggggg")
@@ -858,23 +1201,35 @@ elif tree == None:
     tree = input()
 
 root = tree.getroot()
+if (not re.match('^IPPcode21$', root.attrib['language']) or
+    root.tag != "program"):
+    exit(STRUCT_ERR)
 
 for child in root:
+    #print(type(child.attrib["order"]))
+    #print(child.attrib["order"])
     dict_args = dict()
+    if (child.tag != "instruction" or "opcode" not in child.attrib or
+        "order" not in child.attrib or len(child.attrib) != 2):
+        exit(STRUCT_ERR)
+
+    check_order(prg, child.attrib["order"], prg.get_num_of_instrs())
     prg.set_num_of_instrs(prg.get_num_of_instrs() + 1)
-    check_order(prg, child.attrib["order"])
     idx = 0
+
     for ch in child:
+        if ch.tag != "arg" + str(idx+1) or idx > 2:
+            exit(STRUCT_ERR)
+
         dict_args[idx] = {ch.attrib["type"] : ch.text}
         idx += 1
 
     instr = Factory.resolve(child.attrib["opcode"], dict_args)
-    try:
+    if len(dict_args) > 0:
         check_label(prg, child.attrib["opcode"], dict_args[0])
-    except:
-        pass
 
 while prg.get_line() < prg.get_num_of_instrs():
     Instruction.get_instr_list()[prg.get_line()].execute(prg)
     inc_insts(prg, Instruction.get_instr_list()[prg.get_line()]._opcode)
+    #print(prg.get_line())
     prg.set_line(prg.get_line() + 1)
