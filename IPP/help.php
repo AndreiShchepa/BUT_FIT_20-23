89573a39468e87127d9813250f8ee0018392973e
Shchepa <andrewbandit228@gmail.com>
2022-03-07 21:21:13 +0100

    parse.php final version

--- IPP/help.php
+++ IPP/help.php
@@ -1,17 +1,16 @@
 <?php
 
-# xplagiat00 (change)
 function printHelp() {
-    echo "Script parse.php loads IPPcode19 source code from standard input,\n".
+    echo "Script parse.php loads IPPcode22 source code from standard input,\n".
 		 "checks lexical and syntactic rules and prints XML representation\n".
          "of program to standard output.\n".
 		  "Usage: php parse.php [--help] [--stats=file] [--loc] [--comments] [--labels] [--jumps]\n".
-		    "\t`--help` prints help to standard output.\n".
-		    "\t`--stats=file` prints statistics to the file.\n".
-		    "\t`--loc` prints number of lines with instructions to statistics.\n".
-		    "\t`--comments` prints number of lines with comments to statistics.\n".
-		    "\t`--labels` prints number of labels in code to statistics.\n".
-		    "\t`--jumps` prints number of jumps in code to statistics.\n";
+		    "\t--help prints help to standard output.\n".
+		    "\t--stats=file prints statistics to the file.\n".
+		    "\t--loc prints number of lines with instructions to statistics.\n".
+		    "\t--comments prints number of lines with comments to statistics.\n".
+		    "\t--labels prints number of labels in code to statistics.\n".
+		    "\t--jumps prints number of jumps in code to statistics.\n";
     exit(0);
 }
 
